f4f15d19648556bb5875fc52b0990e3ee362866b
Martim Silva <martimtsilva@hotmail.com>
2022-10-06 21:33:13 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- easy components usage, already shipped with bootstrap css-->
+  <b-container>
+    <!-- try to remove :fields=”fields” to see the magic -->
+    <b-table striped over :items="students" :fields="fields" />
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: ['username', 'name', 'email'],
+      students: []
+    }
+  },
+
+  created() {
+    this.$axios.$get('/api/students').then((students) => {
+      this.students = students
+    })
+  }
+}
+</script>
